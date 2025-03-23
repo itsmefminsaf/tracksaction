9f924d1e4cf15218fac1291edd1ee756acd40b12
itsmefminsaf <itsmefminsaf@gmail.com>
2025-03-23 09:18:21 +0530

    feat: created sign-up api

---
+++ src/utils/connectToMongoDB.ts
@@ -0,0 +1,22 @@
+import { MongoClient } from "mongodb";
+
+const MONGODB_URI = process.env.MONGODB_URI;
+
+if (!MONGODB_URI) {
+  throw new Error("MONGODB_URI is not defined in .env file");
+}
+
+let cachedClient: MongoClient | null = null;
+
+const connectToDatabase = async () => {
+  if (cachedClient) return cachedClient;
+
+  const client = new MongoClient(MONGODB_URI);
+  await client.connect();
+  cachedClient = client;
+  console.log(cachedClient);
+
+  return client;
+};
+
+export default connectToDatabase;
