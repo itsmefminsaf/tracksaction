0d3e728adb9ab6df254ecc58cc1f4a73d6dde442
itsmefminsaf <itsmefminsaf@gmail.com>
2025-03-24 17:49:02 +0530

    feat: Implement session-based authentication check in page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,43 @@
+"use client";
+
+import { useEffect, useState } from "react";
+
 const Root = () => {
-  return <h1>Tracksaction</h1>;
+  const [loading, setLoading] = useState(true);
+  const [user, setUser] = useState(false);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    if (!token) {
+      setLoading(false);
+    } else {
+      fetch("/api/auth/verify-token", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({ token }),
+      })
+        .then((res) => res.json())
+        .then((data) => {
+          if (!data.uid) {
+            setLoading(false);
+          }
+          setUser(true);
+          setLoading(false);
+        });
+    }
+  }, []);
+
+  return (
+    <>
+      {loading ? (
+        <h1>Loading...</h1>
+      ) : user ? (
+        <h1>App</h1>
+      ) : (
+        <h1>Landing Page</h1>
+      )}
+    </>
+  );
 };
 
 export default Root;
