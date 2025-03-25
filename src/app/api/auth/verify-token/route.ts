bc81fa9c32865894a68bbd1762253035499ba718
itsmefminsaf <itsmefminsaf@gmail.com>
2025-03-25 11:14:19 +0530

    feat: implimented verify-token api

--- src/app/api/auth/verify-token/route.ts
+++ src/app/api/auth/verify-token/route.ts
@@ -1,32 +1,29 @@
 import { NextRequest, NextResponse } from "next/server";
 import jwt from "jsonwebtoken";
 
-export const POST = async (req: NextRequest) => {
+export const GET = (req: NextRequest) => {
+  const authHeader = req.headers.get("authorization");
+
+  if (!authHeader || !authHeader.startsWith("Bearer ")) {
+    return NextResponse.json(
+      { message: "Bearer token expected" },
+      { status: 400 },
+    );
+  }
+
+  const token = authHeader.split(" ")[1];
+
   try {
-    const { token } = await req.json();
+    jwt.verify(token, process.env.JWT_PRIVATE_KEY as jwt.Secret) as {
+      id: string;
+    };
 
-    if (!token) {
-      return NextResponse.json(
-        { message: "Where is the token" },
-        { status: 400 },
-      );
-    }
-
-    const tokenPayload = jwt.verify(
-      token,
-      process.env.JWT_PRIVATE_KEY as jwt.Secret,
-    ) as { _id: string };
-
-    if (!tokenPayload) {
-      return NextResponse.json({ message: "Invalid token" }, { status: 401 });
-    }
-
-    return NextResponse.json({ uid: tokenPayload._id }, { status: 200 });
+    return NextResponse.json({ message: "Verified" }, { status: 200 });
   } catch (err) {
     console.log(err);
     return NextResponse.json(
-      { message: "Something went wrong" },
-      { status: 500 },
+      { message: "Invalid or expired token" },
+      { status: 401 },
     );
   }
 };
