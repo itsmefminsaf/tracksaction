1dadd8d86c309d607a54da8d33b3f6d9beb90ad7
itsmefminsaf <itsmefminsaf@gmail.com>
2025-03-28 00:00:06 +0530

    reset

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,41 +1,5 @@
-"use client";
-
-import { useEffect, useState } from "react";
-
 const RootPage = () => {
-  const [loading, setLoading] = useState(true);
-  const [user, setUser] = useState(false);
-
-  useEffect(() => {
-    const token = localStorage.getItem("token");
-    if (!token) {
-      setLoading(false);
-    } else {
-      fetch("/api/auth/verify-token", {
-        method: "GET",
-        headers: {
-          authorization: `Bearer ${token}`,
-        },
-      }).then((res) => {
-        if (res.status === 200) {
-          setUser(true);
-        }
-        setLoading(false);
-      });
-    }
-  }, []);
-
-  return (
-    <>
-      {loading ? (
-        <h1>Loading...</h1>
-      ) : user ? (
-        <h1>App</h1>
-      ) : (
-        <h1>Landing Page</h1>
-      )}
-    </>
-  );
+  return <div>RootPage</div>;
 };
 
 export default RootPage;
