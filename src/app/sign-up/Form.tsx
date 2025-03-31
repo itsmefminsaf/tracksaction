7baa6b5ce00675f2d79a7b8a8f516fbfdf194cfb
itsmefminsaf <itsmefminsaf@gmail.com>
2025-03-31 11:03:48 +0530

    change: email Sign up fields inserted with name field. useActionState hook used

--- src/app/sign-up/Form.tsx
+++ src/app/sign-up/Form.tsx
@@ -1,24 +1,52 @@
 "use client";
 
 import Link from "next/link";
+import { useActionState } from "react";
 import { FaUser } from "react-icons/fa";
 import { PiPasswordBold } from "react-icons/pi";
+import signUpAction from "./action";
+import { formState } from "@/utils/types";
+import { MdEmail } from "react-icons/md";
 
 const Form = () => {
+  const initialState: formState = {
+    values: {},
+  };
+  const [state, action, isPending] = useActionState(signUpAction, initialState);
   return (
-    <form className="bg-foreground/40 flex max-w-96 flex-col gap-4 rounded-3xl p-8">
+    <form
+      action={action}
+      className="bg-foreground/40 flex max-w-96 flex-col gap-4 rounded-3xl p-8"
+    >
       <div className="flex flex-col gap-1">
-        <label htmlFor="username" className="form-input-label">
-          User name
+        <label htmlFor="name" className="form-input-label">
+          Name
         </label>
         <div className="form-input">
           <FaUser />
           <input
             type="text"
-            id="username"
-            name="username"
-            placeholder="Ex: john123"
+            id="name"
+            name="name"
+            placeholder="Ex: SAF shop"
             className="outline-none"
+            defaultValue={state?.values.name}
+          />
+        </div>
+      </div>
+      <div className="flex flex-col gap-1">
+        <label htmlFor="email" className="form-input-label">
+          Email
+        </label>
+        <div className="form-input">
+          <MdEmail />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="Ex: name@mail.com"
+            className="outline-none"
+            defaultValue={state?.values.email}
           />
         </div>
       </div>
@@ -34,6 +62,7 @@ const Form = () => {
             name="password"
             placeholder="Ex: **********"
             className="outline-none"
+            defaultValue={state?.values.password}
           />
         </div>
       </div>
@@ -49,20 +78,26 @@ const Form = () => {
             name="confirmPassword"
             placeholder="Ex: **********"
             className="outline-none"
+            defaultValue={state.values.confirmPassword}
           />
         </div>
       </div>
+      {state.error && (
+        <p className="w-fit place-self-center rounded-full bg-red-500 px-2 py-1 text-xs">
+          {state.error}
+        </p>
+      )}
       <div className="flex items-center justify-evenly">
         <button
           type="submit"
-          className="bg-foreground text-background w-fit rounded-2xl px-3 py-2 font-bold"
+          className="bg-foreground text-background disabled:bg-background/50 w-fit rounded-2xl px-3 py-2 font-bold"
+          disabled={isPending}
         >
-          Sign up
+          {isPending ? "SIgning in" : "Sing in"}
         </button>
         <p className="font-extrabold">or</p>
         <Link
           href={"/sign-in"}
-          type="submit"
           className="bg-background text-foreground w-fit rounded-2xl px-3 py-2 font-bold"
         >
           Sign in
