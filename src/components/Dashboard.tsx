68358d4a5075615711dd0a837076b1256b763fa2
itsmefminsaf <itsmefminsaf@gmail.com>
2025-04-01 20:44:49 +0530

    fix(dashboard): unexciting user.email replaced by email

--- src/components/Dashboard.tsx
+++ src/components/Dashboard.tsx
@@ -5,7 +5,7 @@ const Dashboard = async ({ email }: { email: string }) => {
   return (
     <div>
       <h1>{user?.name}</h1>
-      <h1>{user?.email}</h1>
+      <h1>{email}</h1>
     </div>
   );
 };
