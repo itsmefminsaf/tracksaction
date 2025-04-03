ebe1dc22699e067f6f34cb5feb7af45bd3c4463c
itsmefminsaf <itsmefminsaf@gmail.com>
2025-04-03 11:31:12 +0530

    refactor(dashboard page): remove styles

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,6 +1,4 @@
 import getUserInfo from "@/actions/getUserInfo";
-import Image from "next/image";
-import logo from "@/assets/logo.svg";
 import { redirect } from "next/navigation";
 
 const Dashboard = async () => {
@@ -9,23 +7,9 @@ const Dashboard = async () => {
     redirect("/sign-in");
   }
   return (
-    <>
-      <header className="bg-foreground/50 m-3 flex items-center justify-between rounded-2xl p-5">
-        <div className="flex items-center gap-2">
-          <Image
-            src={logo}
-            alt="Tracksaction"
-            width={64}
-            className="aspect-square"
-          />
-          <h1 className="text-4xl font-extrabold">Tracksaction</h1>
-          <hr className="mx-4 h-10 w-0.5 rotate-12 bg-white" />
-          <h1 className="text-5xl font-bold">Dashboard</h1>
-          <hr className="mx-4 h-10 w-0.5 rotate-12 bg-white" />
-          <h1 className="text-4xl">{user?.name}</h1>
-        </div>
-      </header>
-    </>
+    <header>
+      <h1>{user?.name}</h1>
+    </header>
   );
 };
 
